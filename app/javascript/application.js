ff454af57514ea207a987eab0e40eb9e19d2f0b5
Justine Di Caro <justinedicaro@gmail.com>
2023-01-31 10:37:41 +0100

    frontfront

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,12 +2,4 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import "bootstrap"
-
-new Typed('#typed',{
-  strings : ['To Move', 'To Run', 'To Cycle', 'To Trail'],
-  typeSpeed : 30,
-  startDelay: 2000,
-  backSpeed: 20,
-  backDelay: 2000,
-  loop : true,
-});
+import "home.js"
---
+++ app/javascript/home.js
@@ -0,0 +1,8 @@
+new Typed('#typed',{
+  strings : ['to Move', 'to Run', 'to Cycle', 'to Trail'],
+  typeSpeed : 20,
+  startDelay: 50,
+  backSpeed: 20,
+  backDelay: 1000,
+  loop : true,
+});
